c258062dc53ddaec64c649a71f109bd420cbb2bf
Isreal6ix <isrealsix@gmail.com>
2021-07-05 01:14:31 +0700

    worked on the progress bar and section-3 hover effect

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 let counterA = 0;
 let counterB = 1;
+let boolWatch = true;
+
 const sections = document.querySelectorAll('section');
+const progress = document.querySelector('.progress-box h2');
+const circles = document.querySelectorAll('.circle');
+const [leftBtn, rightBtn] = document.querySelectorAll(
+	'.page-btn--left, .page-btn--right'
+);
+
+const progressCounter = () => {
+	const total = sections.length;
+
+	progress.textContent = `${counterB}/${total}`;
+
+	circles.forEach(circle => {
+		circle.style.backgroundColor = 'transparent';
+	});
+
+	document.querySelector(`.circle-${counterB}`).style.backgroundColor = '#ddd';
+};
+
+const pageController = () => {
+	boolWatch = true;
+	if (counterA === 5) {
+		sections.forEach(section => {
+			section.style.left = '0';
+		});
+		counterA = 0;
+		counterB = 1;
+		progressCounter();
+		boolWatch = false;
+	}
+
+	if (counterA < 0) {
+		sections.forEach(section => {
+			if (section.classList[0] === 'section-5') return;
+			section.style.left = '-100vw';
+		});
+		counterA = 4;
+		counterB = 5;
+		progressCounter();
+		boolWatch = false;
+	}
+	progressCounter();
+	return boolWatch;
+};
 
 window.addEventListener('wheel', e => {
 	const deltaY = e.deltaY > 0;
@@ -12,27 +57,35 @@ window.addEventListener('wheel', e => {
 		counterB--;
 	}
 
-	if (counterA === 5) {
-		sections.forEach(section => {
-			section.style.left = '0';
-		});
-		counterA = 0;
-		counterB = 1;
-		return;
-	}
+	pageController();
+	progressCounter();
 
-	if (counterA < 0) {
-		sections.forEach(section => {
-			if (section.classList[0] === 'section-5') return;
-			section.style.left = '-100vw';
-		});
-		counterA = 4;
-		counterB = 5;
-	}
-
-	document.querySelector(
-		`.section-${deltaY ? counterA : counterB}`
-	).style.left = `${deltaY ? '-100vw' : 0}`;
+	boolWatch &&
+		(document.querySelector(
+			`.section-${deltaY ? counterA : counterB}`
+		).style.left = `${deltaY ? '-100vw' : 0}`);
 
 	console.log(e);
+});
+
+leftBtn.addEventListener('click', () => {
+	counterA--;
+	counterB--;
+	pageController() &&
+		(document.querySelector(`.section-${counterB}`).style.left = 0);
+});
+
+rightBtn.addEventListener('click', () => {
+	counterA++;
+	counterB++;
+	pageController() &&
+		(document.querySelector(`.section-${counterA}`).style.left = '-100vw');
+});
+
+document.querySelector('.grapes-img').addEventListener('mouseover', () => {
+	document.querySelector('.section-3-wrapper').style.opacity = '.5';
+});
+
+document.querySelector('.grapes-img').addEventListener('mouseleave', () => {
+	document.querySelector('.section-3-wrapper').style.opacity = '1';
 });
